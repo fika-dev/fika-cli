cbeb28cf1c53baa65fb7d198b3fcbaf5770fb667
Wonmo Jung <jungcro@gmail.com>
2022-04-19 20:24:09 +0900

    [add] init command with connect option

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-import { pushCommand } from './command/push';
 import { program } from 'commander';
+import { pushCommand } from './command/push';
+import { initCommand } from './command/init';
+
+
 
 
 program.addCommand(pushCommand);
+program.addCommand(initCommand);
 program.parse(process.argv);
