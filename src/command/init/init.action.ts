4b75ab3b8c14538f8a0cc429b62144c3963d3fb1
Wonmo Jung <jungcro@gmail.com>
2022-04-21 14:03:24 +0900

    [add] init action

---
+++ src/command/init/init.action.ts
@@ -0,0 +1,8 @@
+import SERVICE_IDENTIFIER from "src/config/constants/identifiers";
+import container from "src/config/ioc_config";
+import { IConfigService } from "src/domain/service/i_config.service";
+
+export const initAction = ()=>{
+  const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
+  configService.createConfig();
+}
