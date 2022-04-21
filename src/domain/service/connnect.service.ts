da7548f099c6111eea2bf61e09090144548cfc6e
Wonmo Jung <jungcro@gmail.com>
2022-04-21 18:36:51 +0900

    Revert "Feature/iss 6"

--- src/domain/service/connnect.service.ts
+++ src/domain/service/connnect.service.ts
@@ -2,8 +2,6 @@ import { DevObject } from "../entity/dev_object.entity";
 import { NotionWorkspace } from "../entity/notion_workspace.entity";
 import { IConnectService } from "./i_connect.service";
 import open from 'open';
-import axios, { AxiosError } from "axios";
-import { CreateNotionWorkspaceDto, CreateNotionWorkspaceDtoType } from "src/infrastructure/dto/create_notion_workspace.dto";
 
 export class ConnectService implements IConnectService {
   create(devObj: DevObject): Promise<string> {
@@ -21,16 +19,7 @@ export class ConnectService implements IConnectService {
     const targetUri = `${notionAuthorizeUri}?${params}`;
     await open(targetUri);
   }
-  async requestNotionWorkspace(): Promise<NotionWorkspace> {
-    try{
-      const response = await axios.get('https://fikaapi.kkiri.app/notion/workspace?id=0aefa0c0-ceed-4158-be40-6dfc3901770e');
-      const dto = new CreateNotionWorkspaceDto(response.data as CreateNotionWorkspaceDtoType);
-      return dto.toEntity();
-    }catch(e){
-      const axiosError = e as AxiosError;
-      console.log('🧪', ' in ConnnectService: ', 'error code: ',e.code);
-      throw new Error(e.response?.message);
-    }
-    
+  requestNotionWorkspace(): NotionWorkspace {
+    throw new Error("Method not implemented.");
   }
 }
