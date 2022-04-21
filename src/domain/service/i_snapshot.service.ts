ea120c970b10f1a5f87e29e9a0139361f7d77975
Wonmo Jung <jungcro@gmail.com>
2022-04-21 16:23:12 +0900

    [fix] snapshot service to return difference

--- src/domain/service/i_snapshot.service.ts
+++ src/domain/service/i_snapshot.service.ts
@@ -1,6 +1,13 @@
+import { DevObject } from "../entity/dev_object.entity";
 import { Snapshot } from "../entity/snapshot.entity";
+
+export interface Difference {
+  toBeCreated: DevObject[],
+  toBeUpdated: DevObject[],
+  toBeRemoved: DevObject[],
+}
 
 export interface ISnapshotService {
   saveSyncedSnapshot(snapshot: Snapshot);
-  compare(analyzedSnapshot: Snapshot);
+  compare(analyzedSnapshot: Snapshot): Difference;
 }
--- src/domain/service/snapshot.service.ts
+++ src/domain/service/snapshot.service.ts
@@ -1,9 +1,11 @@
 import { Snapshot } from "../entity/snapshot.entity";
 import { SyncedSnapshot } from "../entity/synced_snapshot.entity";
-import { ISnapshotService } from "./i_snapshot.service";
+import { Difference, ISnapshotService } from "./i_snapshot.service";
 
 export class SnapshotService implements ISnapshotService{
   private _recentSnapshot: SyncedSnapshot | undefined;
   public saveSyncedSnapshot(snapshot: Snapshot){}
-  public compare(analyzedSnapshot: Snapshot){}
+  public compare(analyzedSnapshot: Snapshot): Difference {
+    throw new Error("Method not implemented.");
+  }
 }
