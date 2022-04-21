ff65dded2c7781440c41a583e4b5d54ee7a79601
Wonmo Jung <jungcro@gmail.com>
2022-04-21 16:18:29 +0900

    [replace] push -> create, update, remove

--- src/domain/service/connnect.service.ts
+++ src/domain/service/connnect.service.ts
@@ -1,14 +1,21 @@
+import { DevObject } from "../entity/dev_object.entity";
 import { NotionWorkspace } from "../entity/notion_workspace.entity";
 import { IConnectService } from "./i_connect.service";
 
 export class ConnectService implements IConnectService {
+  create(devObj: DevObject): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+  update(devObj: DevObject): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+  remove(devObj: DevObject): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
   guideNotionAuthentication(): void {
     throw new Error("Method not implemented.");
   }
   requestNotionWorkspace(): NotionWorkspace {
-    throw new Error("Method not implemented.");
-  }
-  push(): Promise<void> {
     throw new Error("Method not implemented.");
   }
 }
--- src/domain/service/i_connect.service.ts
+++ src/domain/service/i_connect.service.ts
@@ -1,9 +1,12 @@
+import { DevObject } from "../entity/dev_object.entity";
 import { NotionWorkspace } from "../entity/notion_workspace.entity";
 import { Snapshot } from "../entity/snapshot.entity";
 import { AddOnConfig } from "../value_object/add-on-config.vo";
 
 export interface IConnectService {
   guideNotionAuthentication(): void;
-  requestNotionWorkspace(): NotionWorkspace;
-  push(): Promise<void>;
+  requestNotionWorkspace(botId: string): NotionWorkspace;
+  create(devObj: DevObject): Promise<string>;
+  update(devObj: DevObject): Promise<string>;
+  remove(devObj: DevObject): Promise<string>;
 }
