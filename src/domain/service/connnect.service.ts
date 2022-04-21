936cee12004107bb6e5df12d120afbeb22fc8ba5
Wonmo Jung <jungcro@gmail.com>
2022-04-21 12:50:05 +0900

    [fix] inteface connect service

--- src/domain/service/connnect.service.ts
+++ src/domain/service/connnect.service.ts
@@ -1,4 +1,11 @@
-export class ConnectService {
-  public requestNotionWorkspace(){}
-  public push(){}
+import { NotionWorkspace } from "../entity/notion_workspace.entity";
+import { IConnectService } from "./i_connect.service";
+
+export class ConnectService implements IConnectService {
+  requestNotionWorkspace(): NotionWorkspace {
+    throw new Error("Method not implemented.");
+  }
+  push(): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
 }
--- src/domain/service/i_connect.service.ts
+++ src/domain/service/i_connect.service.ts
@@ -1,7 +1,8 @@
+import { NotionWorkspace } from "../entity/notion_workspace.entity";
 import { Snapshot } from "../entity/snapshot.entity";
 import { AddOnConfig } from "../value_object/add-on-config.vo";
 
 export interface IConnectService {
-  registerAnalyzers(configs: AddOnConfig[]);
-  analyze(): Snapshot;
+  requestNotionWorkspace(): NotionWorkspace;
+  push(): Promise<void>;
 }
