684e043cdad13349346f991a1d582c39f141a826
Wonmo Jung <jungcro@gmail.com>
2022-04-21 18:35:18 +0900

    [impl] updateNotionWorkspace

--- src/domain/service/i_config.service.ts
+++ src/domain/service/i_config.service.ts
@@ -2,9 +2,9 @@ import { NotionWorkspace } from "../entity/notion_workspace.entity";
 import { AddOnConfig } from "../value_object/add_on_config.vo";
 
 export interface IConfigService {
-  createConfig(currentPath: string): Promise<void>;
+  createConfig(currentPath: string): void;
   readConfig(): Promise<void>;
   updateConfig(): Promise<void>;
   getAnalyzerConfigs(): AddOnConfig[];
-  updateNotionWorkspace(notionWorkspace: NotionWorkspace): Promise<void>;
+  updateNotionWorkspace(notionWorkspace: NotionWorkspace): void;
 }
