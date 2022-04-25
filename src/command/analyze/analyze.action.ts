3756898ee472380a686f4546b33dca3ec1e41847
Wonmo Jung <jungcro@gmail.com>
2022-04-25 10:55:11 +0900

    [impl] analyze action

--- src/command/analyze/analyze.action.ts
+++ src/command/analyze/analyze.action.ts
@@ -3,12 +3,21 @@ import container from "src/config/ioc_config";
 import { Snapshot } from "src/domain/entity/snapshot.entity";
 import { IAnalyzeService } from "src/domain/service/i_analyze.service";
 import { IConfigService } from "src/domain/service/i_config.service";
+import { IMorphService } from "src/domain/service/i_morph.service";
 
-export const analyzeAction = () : Snapshot=>{
+export const  analyzeAction = async () : Promise<Snapshot> => {
   const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
   const analyzeService = container.get<IAnalyzeService>(SERVICE_IDENTIFIER.AnalyzeService);
-  const configs = configService.getAnalyzerConfigs();
-  analyzeService.registerAnalyzers(configs);
-  const analyzedSnapshot = analyzeService.analyze();
+  const morphService = container.get<IMorphService>(SERVICE_IDENTIFIER.MorphService);
+
+  configService.readConfig();
+  const analyzerConfigs = configService.getAnalyzerConfigs();
+  analyzeService.registerAnalyzers(analyzerConfigs);
+
+  const morpherConfig = configService.getMorpherConfig();
+  morphService.configMorpher(morpherConfig);
+  const morpher = morphService.getMorpher();
+
+  const analyzedSnapshot = await analyzeService.analyze(morpher);
   return analyzedSnapshot;
 }
