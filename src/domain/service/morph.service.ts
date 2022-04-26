acb02210b035493d18d9e294610f4b1c7c7c4e09
Wonmo Jung <jungcro@gmail.com>
2022-04-26 19:46:37 +0900

    [impl] configMorpher method

--- src/domain/service/morph.service.ts
+++ src/domain/service/morph.service.ts
@@ -1,12 +1,21 @@
+import { TypescriptMorpher } from "plug_in/morpher/typescript_morpher";
+import { AddOnType } from "../entity/add_on.entity";
 import { Morpher } from "../entity/morpher.entity";
 import { AddOnConfig } from "../value_object/add_on_config.vo";
 import { IMorphService } from "./i_morph.service";
 
 export class MorphService implements IMorphService{
   private _morpher: Morpher | undefined;
+  constructor(){
+    this.configMorpher = this.configMorpher.bind(this);
+  }
   
   configMorpher(config: AddOnConfig): void {
-    throw new Error("Method not implemented.");
+    if (config.type === AddOnType.Morpher){
+      if (config.name === 'Typescript.Morpher'){
+        this._morpher = new TypescriptMorpher(config);
+      }
+    }
   }
   getMorpher(): Morpher {
     if (this._morpher){
