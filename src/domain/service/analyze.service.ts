f4266222362c859a5c082748d11e70b8bc7f69b3
Wonmo Jung <jungcro@gmail.com>
2022-04-26 19:43:20 +0900

    [impl] registerAnalyzers method

--- src/domain/service/analyze.service.ts
+++ src/domain/service/analyze.service.ts
@@ -1,3 +1,5 @@
+import { GitRepoAnalyzer } from "plug_in/analyzer/git_repo_analyzer";
+import { ReactComponentAnalyzer } from "plug_in/analyzer/react_component_analyzer";
 import { Analyzer } from "../entity/analyzer.entity";
 import { Component } from "../entity/component.entity";
 import { ObjectType } from "../entity/dev_object.entity";
@@ -8,8 +10,24 @@ import { AddOnConfig } from "../value_object/add_on_config.vo";
 import { IAnalyzeService } from "./i_analyze.service";
 
 export class AnalyzeService implements IAnalyzeService{
+  constructor(){
+    this.registerAnalyzers = this.registerAnalyzers.bind(this);
+  }
   private _analyzers: Analyzer[] = [];
-  public registerAnalyzers(configs: AddOnConfig[]){}
+  public registerAnalyzers(configs: AddOnConfig[]){
+    this._analyzers = configs.map(config=>{
+      if (config.objectType === ObjectType.Repo){
+        return new GitRepoAnalyzer(config);
+      }
+      if (config.objectType === ObjectType.Component){
+        if (config.name === "React.Component" ){
+          return new ReactComponentAnalyzer(config);
+        }
+      }else{
+        return undefined;
+      }
+    }).filter(analyzer => analyzer);
+  }
   public async analyze(morpher: Morpher): Promise<Snapshot> {
     const devObjs = (await Promise.all(
       this._analyzers.map(
