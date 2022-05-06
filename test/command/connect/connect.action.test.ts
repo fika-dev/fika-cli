b538cf257cc07ad197a12dfe56dcb13a1d57172c
Wonmo Jung <jungcro@gmail.com>
2022-05-06 10:42:46 +0900

    [test] [add] connect action test

---
+++ test/command/connect/connect.action.test.ts
@@ -0,0 +1,28 @@
+import { initAction } from "src/command/init/init.action";
+import { setAction } from "src/command/set/set.action";
+import container from "src/config/ioc_config";
+import { NotionWorkspace } from "src/domain/entity/notion_workspace.entity";
+import { TEST_FIKA_BOT_ID } from "test/test-constants";
+import { readTestFikaConfig } from "test/test-utils";
+import SERVICE_IDENTIFIER from "src/config/constants/identifiers";
+import { IConfigService } from "src/domain/service/i_config.service";
+import { IConnectService } from "src/domain/service/i_connect.service";
+import axios from "axios";
+
+beforeAll(()=>{
+  
+});
+
+beforeEach(()=>{
+  container.snapshot();
+});
+
+afterEach(()=>{
+  container.restore();
+});
+
+test('1. guide notion authentication', async () => { 
+  const uri = container.get<IConnectService>(SERVICE_IDENTIFIER.ConnectService).getNotionAuthenticationUri();
+  const response = await axios.get(uri);
+  expect(response.status).toEqual(200);
+});
