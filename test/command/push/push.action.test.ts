0021d6c0d26c03f6bc5bcd198967d8b22ed21b36
Wonmo Jung <jungcro@gmail.com>
2022-05-06 18:29:23 +0900

    [test] [add] push action

--- test/command/push/push.action.test.ts
+++ test/command/push/push.action.test.ts
@@ -1,18 +1,27 @@
+import { analyzeAction } from "src/command/analyze/analyze.action";
 import { initAction } from "src/command/init/init.action";
+import { pushAction } from "src/command/push/push.action";
 import SERVICE_IDENTIFIER from "src/config/constants/identifiers";
 import container from "src/config/ioc_config";
-import { clearTestFikaPath } from "test/test-utils";
+import { DevObject } from "src/domain/entity/dev_object.entity";
+import { clearTestFikaPath, readTestSnapshot } from "test/test-utils";
+
+const SAMPLE_1_PATH =  process.cwd()+'/test/test-samples/sample_1'
 
 beforeEach(()=>{
-  clearTestFikaPath(process.cwd()+'/test/test-samples/sample_1');
-  initAction(process.cwd()+'/test/test-samples/sample_1');
+  clearTestFikaPath(SAMPLE_1_PATH);
+  initAction(SAMPLE_1_PATH);
 });
 
 test('', 
-  ()=>{
+  async ()=>{
     container.unbind(SERVICE_IDENTIFIER.ConnectService);
     let connectionServiceMock = {
-      
+      create: (devObj: DevObject) => 'sample_test_uri'
     }
-
+    container.bind<any>(SERVICE_IDENTIFIER.ConnectService).toConstantValue(connectionServiceMock);
+    const snapshot = await analyzeAction();
+    await pushAction(snapshot, SAMPLE_1_PATH);
+    const syncedSnapshot = readTestSnapshot(SAMPLE_1_PATH);
+    expect(syncedSnapshot.components.length).toBeGreaterThan(0);
 })
