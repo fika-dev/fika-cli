6c37f6404087ba58e9184b7134a2d2f41c950c71
Wonmo Jung <jungcro@gmail.com>
2022-05-06 18:29:37 +0900

    [test] [add] readTestSnapshot util

--- test/test-utils/index.ts
+++ test/test-utils/index.ts
@@ -1,5 +1,8 @@
 import fs from "fs"
+import path from "path";
+import { FIKA_PATH, SNAPSHOT_FILE_NAME } from "src/config/constants/path";
 import { Config } from "src/domain/entity/config.entity";
+import { SyncedSnapshot } from "src/domain/entity/synced_snapshot.entity";
 
 export const clearTestFikaPath = (currentPath: string)=>{
   const fikaPath = currentPath + '/.fika';
@@ -18,5 +21,16 @@ export const readTestFikaConfig = (currentPath: string): Config=>{
     return config;
   }else{
     throw new Error("Fika config file path is not set");
+  }
+}
+
+export const readTestSnapshot = (currentPath: string): SyncedSnapshot=>{
+  const fikaSnapshotFilePath = path.join(currentPath, FIKA_PATH, SNAPSHOT_FILE_NAME );
+  if (fikaSnapshotFilePath){
+    const snapshotString = fs.readFileSync(fikaSnapshotFilePath, 'utf-8');
+    const snapshot = JSON.parse(snapshotString) as SyncedSnapshot;
+    return snapshot;
+  }else{
+    throw new Error("Fika snapshot file path is not set");
   }
 }
