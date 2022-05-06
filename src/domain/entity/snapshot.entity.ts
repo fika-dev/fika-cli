72907edb87d06e72b3ea3cec40432ee076a02b61
Wonmo Jung <jungcro@gmail.com>
2022-05-06 16:57:28 +0900

    [add] getDevObjects method

--- src/domain/entity/snapshot.entity.ts
+++ src/domain/entity/snapshot.entity.ts
@@ -1,7 +1,17 @@
 import { Repo } from "./repo.entity";
 import { Component } from "./component.entity";
+import { DevObject } from "./dev_object.entity";
 
 export class Snapshot {
-  repo: Repo;
+  repo?: Repo;
   components: Component[]
+  getDevObjects(): DevObject[]{
+    if (this.repo){
+      return [this.repo].concat(this.components)
+    }
+    else{
+      return this.components;
+    }
+  }
+  
 }
