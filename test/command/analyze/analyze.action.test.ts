1e43332c08a8576aaeb442a5a674a2771db4f081
Wonmo Jung <jungcro@gmail.com>
2022-05-09 14:56:05 +0900

    [test] [update] test sample 01 path

--- test/command/analyze/analyze.action.test.ts
+++ test/command/analyze/analyze.action.test.ts
@@ -2,13 +2,15 @@ import { analyzeAction } from "src/command/analyze/analyze.action";
 import { initAction } from "src/command/init/init.action";
 import { clearTestFikaPath } from "test/test-utils";
 
+const SAMPLE_1_PATH =  process.cwd()+'/test/test-samples/sample_01'
+
 beforeAll(()=>{
   
 });
 
 beforeEach(()=>{
-  clearTestFikaPath(process.cwd()+'/test/test-samples/sample_1');
-  initAction(process.cwd()+'/test/test-samples/sample_1');
+  clearTestFikaPath(SAMPLE_1_PATH);
+  initAction(SAMPLE_1_PATH);
 });
 
 afterEach(()=>{
