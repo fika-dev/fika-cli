d190c4639a5c9da84ad8d78a3e3e55d1b7c36d04
Wonmo Jung <jungcro@gmail.com>
2022-05-10 10:32:50 +0900

    [add] GitPlatform entity

---
+++ src/domain/entity/git_platform.entity.ts
@@ -0,0 +1,8 @@
+import { AddOn } from "./add_on.entity";
+import { DevObject } from "./dev_object.entity";
+import { Issue } from "./issue.entity";
+import { Morpher } from "./morpher.entity";
+
+export abstract class GitPlatform extends AddOn{
+  abstract createIssue(issue: Issue): Promise<Issue>;
+}
