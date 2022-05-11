f49e0d24af49e294515b68cd30940a1601241eb6
Wonmo Jung <jungcro@gmail.com>
2022-05-11 16:50:14 +0900

    [add] connect command

---
+++ src/command/connect/index.ts
@@ -0,0 +1,12 @@
+// import path from 'path';
+import { Command } from 'commander';
+import open from 'open';
+import { connectAction } from './connect.action';
+
+export const connectCommand = new Command()
+  .command('connect [remoteName]')
+  .description('connect')
+  // .option('-p, --port <number>', 'port to run server on', '4005')
+  .action( async (remoteName = 'org', options: { port: string }) => {
+    connectAction();
+  });
