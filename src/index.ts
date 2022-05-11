2b7b4e2f91f3d265a391cd11488803b011d15984
Wonmo Jung <jungcro@gmail.com>
2022-05-11 17:29:50 +0900

    [add] commander name, desc, version

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { initCommand } from '@/command/init';
 import { pushCommand } from '@/command/push';
 import { connectCommand } from '@/command/connect';
 
+program.name('fika')
+  .description('CLI for advanced your workflow')
+  .version('0.1.0');
+
 program.addCommand(pushCommand);
 program.addCommand(initCommand);
 program.addCommand(connectCommand);
