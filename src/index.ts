6ecd54048f6c9f200f974b521870cc01c27feb19
Wonmo Jung <jungcro@gmail.com>
2022-05-13 10:18:39 +0900

    [add] create command & rm init command

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
-import { program } from 'commander';
-import { initCommand } from '@/command/init';
-import { pushCommand } from '@/command/push';
 import { connectCommand } from '@/command/connect';
+import { pushCommand } from '@/command/push';
+import { createCommand } from '@/command/create';
+import { program } from 'commander';
 
 program.name('fika')
   .description('CLI for advanced your workflow')
-  .version('0.1.0');
+  .version('0.1.3');
 
 program.addCommand(pushCommand);
-program.addCommand(initCommand);
+program.addCommand(createCommand);
 program.addCommand(connectCommand);
 program.parse(process.argv);
