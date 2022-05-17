a6278148215b88a39b7a82f7aba6cfe1230465f5
Wonmo Jung <jungcro@gmail.com>
2022-05-17 11:31:30 +0900

    [add] COMMAND_NOT_FOUND_STRING const string

--- src/domain/value_object/exceptions/no_gh_cli.ts
+++ src/domain/value_object/exceptions/no_gh_cli.ts
@@ -1,5 +1,6 @@
-class NoGithubCli extends Error{
-  constructor(name: string, message?: string){
+export const COMMAND_NOT_FOUND_STRING = 'command not found';
+export class NoGithubCli extends Error{
+  constructor(name?: string, message?: string){
     super();
     this.name = name;
     if (message){
