f74d20b1548a51c45d10a7c8fd83bac509aafb33
Wonmo Jung <jungcro@gmail.com>
2022-05-17 19:14:56 +0900

    [add] wrap entire program with error handling

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,22 @@ import { pushCommand } from '@/command/push';
 import { createCommand, createIssueShortCommand, createPRShortCommand } from '@/command/create';
 import { program } from 'commander';
 import { setCommand } from './command/set';
+import container from './config/ioc_config';
+import { IErrorHandlingService } from './domain/service/i_error_handling.service';
+import SERVICE_IDENTIFIER from './config/constants/identifiers';
 
-program.name('fika')
+try{
+  program.name('fika')
   .description('CLI for advanced your workflow')
   .version('0.1.3');
-
-program.addCommand(pushCommand);
-program.addCommand(createCommand);
-program.addCommand(createIssueShortCommand);
-program.addCommand(createPRShortCommand);
-program.addCommand(connectCommand);
-program.addCommand(setCommand);
-program.parse(process.argv);
+  program.addCommand(pushCommand);
+  program.addCommand(createCommand);
+  program.addCommand(createIssueShortCommand);
+  program.addCommand(createPRShortCommand);
+  program.addCommand(connectCommand);
+  program.addCommand(setCommand);
+  program.parse(process.argv);
+}catch(e){
+  const errorHandlingService = container.get<IErrorHandlingService>(SERVICE_IDENTIFIER.ErrorHandlingService);
+  errorHandlingService.handle(e);
+}
