f86033ba7f164a161937796542c2d85d67c4b40e
Wonmo Jung <jungcro@gmail.com>
2022-05-17 14:36:40 +0900

    [add] error handling of GH_CLI_NOT_LOGGEDIN

--- plug_in/git_platform/git_hub/index.ts
+++ plug_in/git_platform/git_hub/index.ts
@@ -4,7 +4,8 @@ import { Issue } from "src/domain/entity/issue.entity";
 import { AddOnConfig } from "src/domain/value_object/add_on_config.vo";
 import {promisify} from 'util';
 import { exec } from 'child_process';
-import { COMMAND_NOT_FOUND_STRING, NoGithubCli} from 'src/domain/value_object/exceptions/no_gh_cli';
+import { COMMAND_NOT_FOUND_STRING, NoGithubCli} from '@/domain/value_object/exceptions/no_gh_cli';
+import { GHCliNotLoggedin, NEED_LOGIN_STRING } from "@/domain/value_object/exceptions/gh_cli_not_loggedin";
 
 export class GitHub extends GitPlatform{
   
@@ -21,6 +22,9 @@ export class GitHub extends GitPlatform{
       if (stderr.includes(COMMAND_NOT_FOUND_STRING)){
         throw new NoGithubCli('NO_GH_CLI');
       }
+      if (stderr.includes(NEED_LOGIN_STRING)){
+        throw new GHCliNotLoggedin('GH_CLI_NOT_LOGGEDIN');
+      }
     }
     const updatedIssue: Issue = {
       ...issue,
@@ -35,6 +39,14 @@ export class GitHub extends GitPlatform{
     const {stdout: branchName, stderr: branchNameErr} = await execP('git rev-parse --abbrev-ref HEAD');
     const {stdout: pushOut, stderr: pushErr} =await execP(`git push origin ${branchName}`);
     const {stdout, stderr} = await execP(`gh pr create  --title "${issue.title}" --body "${issue.body}" ${labelOptions} --base develop`);
+    if (stderr){
+      if (stderr.includes(COMMAND_NOT_FOUND_STRING)){
+        throw new NoGithubCli('NO_GH_CLI');
+      }
+      if (stderr.includes(NEED_LOGIN_STRING)){
+        throw new GHCliNotLoggedin('GH_CLI_NOT_LOGGEDIN');
+      }
+    }
     const updatedIssue: Issue = {
       ...issue,
       prUrl: stdout.trim(),
