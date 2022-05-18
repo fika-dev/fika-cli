0378695cad71c5071b3e6c8572d6161a85f54be4
Wonmo Jung <jungcro@gmail.com>
2022-05-18 11:15:42 +0900

    [add] wrapper to connect action

--- src/command/connect/index.ts
+++ src/command/connect/index.ts
@@ -1,6 +1,7 @@
 // import path from 'path';
 import { Command } from 'commander';
 import open from 'open';
+import { asyncErrorHandler } from '../error/error_handlers';
 import { connectAction } from './connect.action';
 
 export const connectCommand = new Command()
@@ -8,5 +9,5 @@ export const connectCommand = new Command()
   .description('connect')
   // .option('-p, --port <number>', 'port to run server on', '4005')
   .action( async (remoteName = 'org', options: { port: string }) => {
-    connectAction();
+    asyncErrorHandler(connectAction());
   });
