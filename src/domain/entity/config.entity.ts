db4a97fe53ee410bba4a88596220a35ba0848938
Wonmo Jung <jungcro@gmail.com>
2022-06-03 11:27:36 +0900

    [add] GitConfig to Config and defaultConfig

--- src/domain/entity/config.entity.ts
+++ src/domain/entity/config.entity.ts
@@ -1,9 +1,11 @@
 import { AddOnConfig } from "../value_object/add_on_config.vo";
 import { FikaToken, UnAuthenticated } from "../value_object/fika-token.vo";
+import { GitConfig } from "../value_object/git_config.vo";
 import { NotConnected, NotionWorkspace } from "./notion_workspace.entity";
 
 export class Config {
   notionWorkspace: NotionWorkspace | NotConnected;
   addOns: AddOnConfig[];
   fikaToken: FikaToken | UnAuthenticated;
+  git: GitConfig;
 }
