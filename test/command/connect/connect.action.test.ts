0f7d83c2c56e58bd74c16ae4428eec4b5805d008
Wonmo Jung <jungcro@gmail.com>
2022-07-05 14:34:19 +0900

    [add] checkAndCloneRepo in beforeAll in connection test

--- test/command/connect/connect.action.test.ts
+++ test/command/connect/connect.action.test.ts
@@ -3,9 +3,10 @@ import axios from "axios";
 import SERVICE_IDENTIFIER from "src/config/constants/identifiers";
 import container from "src/config/ioc_config";
 import { IConnectService } from "src/domain/service/i_connect.service";
+import { checkAndCloneRepo } from "test/test-utils";
 
 beforeAll(()=>{
-  
+  checkAndCloneRepo()
 });
 
 beforeEach(()=>{
@@ -24,8 +25,7 @@ test('1. guide notion authentication', async () => {
 
 
 test('2. get notion workspace', async () => { 
-  const botId = new Uuid('80aecc8f-da82-4a8b-bb87-7c594be20c05');
+  const botId = new Uuid('0aefa0c0-ceed-4158-be40-6dfc3901770e');
   const notionWorkspace = await container.get<IConnectService>(SERVICE_IDENTIFIER.ConnectService).requestNotionWorkspace(botId);
-  console.log('🧪', ' in ConnectActionTest: ', 'notionWorkspace: ', notionWorkspace);
   expect(notionWorkspace).toBeDefined();
 });
