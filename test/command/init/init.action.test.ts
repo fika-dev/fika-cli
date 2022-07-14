cc3f44796f7cd6d68d300c15cf78bc6a8c6359e3
Wonmo Jung <jungcro@gmail.com>
2022-07-14 18:50:27 +0900

    [test] [add] 3. create local config file

--- test/command/init/init.action.test.ts
+++ test/command/init/init.action.test.ts
@@ -1,8 +1,10 @@
+import { defaultLocalConfig } from "@/config/constants/default_config";
 import SERVICE_IDENTIFIER from "@/config/constants/identifiers";
 import container from "@/config/ioc_config";
 import { IPromptService } from "@/domain/service/i-prompt.service";
+import { IConfigService } from "@/domain/service/i_config.service";
 import { initAction } from "src/command/init/init.action";
-import { clearTestFikaPath, readTestFikaConfig, sendPromptData } from "test/test-utils";
+import { clearLocalConfig, clearTestFikaPath, readLocalConfig, readTestFikaConfig, sendPromptData } from "test/test-utils";
 jest.spyOn(process.stdout, 'write').mockImplementation(() => true);
 
 afterEach(jest.clearAllMocks);
@@ -11,10 +13,12 @@ afterEach(jest.clearAllMocks);
 
 beforeAll(() => {
   clearTestFikaPath(process.env.TESTING_PATH);
+  clearLocalConfig(process.env.TESTING_REPO_PATH);
 });
 
 afterAll(() => {
   clearTestFikaPath(process.env.TESTING_PATH);
+  clearLocalConfig(process.env.TESTING_REPO_PATH);
 });
 
 test('1. create config & check notion workspace', () => { 
@@ -26,8 +30,16 @@ test('1. create config & check notion workspace', () => {
 test('2. test prompt askBranchName', async () => { 
   const promptService = container.get<IPromptService>(SERVICE_IDENTIFIER.PromptService);
   const branchName = 'develop';
-  sendPromptData(branchName)
+  sendPromptData(branchName);
   const devBranchName = await promptService.askBranchName("name for develop branch", "develop", ["develop"]);
   expect(devBranchName).toBe(branchName);  
   expect(process.stdout.write).toHaveBeenCalledWith("name for develop branch (already defined branches: develop): ");
+});
+
+test('3. create local config file', async () => { 
+  const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
+  configService.createLocalConfig({branchNames: defaultLocalConfig.branchNames});
+  const config = readLocalConfig(process.env.TESTING_REPO_PATH);
+  console.log('🧪', ' in InitActionTest: ', 'config: ',config);
+  expect(config.branchNames.develop).toBe(defaultLocalConfig.branchNames.develop);
 });
