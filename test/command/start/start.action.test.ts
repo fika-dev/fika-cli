57db8871c78a7c030eb18468f4e9ad5b69115f50
Wonmo Jung <jungcro@gmail.com>
2022-07-21 18:02:16 +0900

    [add] uuid

--- test/command/start/start.action.test.ts
+++ test/command/start/start.action.test.ts
@@ -6,6 +6,7 @@ import { IGitPlatformService } from "@/domain/entity/i_git_platform.service";
 import { IPromptService } from "@/domain/service/i-prompt.service";
 import { IConfigService } from "@/domain/service/i_config.service";
 import { IMessageService } from "@/domain/service/i_message.service";
+import { Uuid } from "@/domain/value_object/uuid.vo";
 import { TEST_CHANGE_FILE_PATH, TEST_CPR_BRANCH_NAME, TEST_START_DOC_ID } from "test/test-constants";
 import { checkAndCloneRepo, checkAndDeleteIssue, createTestConfig, makeMeaninglessChange, restoreGitRepo, setAuthToken, setUseToken } from "test/test-utils";
 
@@ -24,6 +25,8 @@ beforeEach(async()=>{
   jest.restoreAllMocks();
   jest.spyOn(messageService, 'showCreatingGitIssue').mockImplementation(()=>{});
   jest.spyOn(messageService, 'showGettingIssue').mockImplementation(()=>{});
+  jest.spyOn(configService, 'getNotionBotId').mockImplementation(()=>new Uuid('d3224eba-6e67-4730-9b6f-a9ef1dc7e4ac'));
+  await gitPlatformService.checkoutToBranchWithoutReset('develop');
   await restoreGitRepo(process.env.TESTING_REPO_PATH);
 });
 
