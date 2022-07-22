a1b00efc3d19cab32d0e8f186ad1e7a9f266b801
Wonmo Jung <jungcro@gmail.com>
2022-07-22 17:02:47 +0900

    [impl] finish action

--- src/command/finish/finish.action.ts
+++ src/command/finish/finish.action.ts
@@ -1,6 +1,6 @@
 import { Issue } from "@/domain/entity/issue.entity";
+import { IPromptService } from "@/domain/service/i-prompt.service";
 import { IMessageService } from "@/domain/service/i_message.service";
-import { NotionUrl } from "@/domain/value_object/notion_url.vo";
 import SERVICE_IDENTIFIER from "src/config/constants/identifiers";
 import container from "src/config/ioc_config";
 import { IGitPlatformService } from "src/domain/entity/i_git_platform.service";
@@ -11,11 +11,35 @@ export const finishAction = async (baseBranch?: string) => {
   const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
   const connectService = container.get<IConnectService>(SERVICE_IDENTIFIER.ConnectService);
   const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
-  messageService.showGettingIssueForPR();
+  const promptService = container.get<IPromptService>(SERVICE_IDENTIFIER.PromptService);
   const gitPlatformConfig = configService.getGitPlatformConfig();
   const gitPlatformService = container.get<IGitPlatformService>(
     SERVICE_IDENTIFIER.GitPlatformService
   );
+  const localConfig = configService.getLocalConfig();
+  const isChangeExist = await gitPlatformService.checkUnstagedChanges();
+  if (isChangeExist) {
+    const proceed = promptService.confirmAction(
+      "There is uncommited changes, Do you wanna proceed to push and create pull request?"
+    );
+    if (!proceed) return;
+  }
+  if (localConfig.finish.checkMergeConflict) {
+    const isUpdated = await gitPlatformService.pullFrom(
+      baseBranch ? baseBranch : localConfig.branchNames.develop
+    );
+    const isConflictExist = await gitPlatformService.checkConflict();
+    if (isConflictExist) {
+      messageService.showWarning("There is conflict. Try again after resolving conflict");
+      return;
+    } else if (isUpdated) {
+      messageService.showSuccess("Successfuly Merged");
+    } else {
+      messageService.showSuccess("Nothing to update");
+    }
+  }
+
+  messageService.showGettingIssueForPR();
   const botId = configService.getNotionBotId();
   const branchName = await gitPlatformService.getBranchName();
   const gitRepoUrl = await gitPlatformService.getGitRepoUrl();
@@ -27,10 +51,17 @@ export const finishAction = async (baseBranch?: string) => {
   await gitPlatformService.pushBranch(branchName);
   gitPlatformService.configGitPlatform(gitPlatformConfig);
   messageService.showCreatingPR(issue, branchName);
+  // [TODO] if base branch
   const updatedIssue = await gitPlatformService.createPR(issue, branchName);
   await connectService.updateIssue(updatedIssue, botId);
   const issueNumber = configService.parseIssueNumber(branchName);
   const prNumber = Issue.parseNumberFromUrl(updatedIssue.prUrl);
+  // [TODO] if base branch
   await connectService.createPullRequest(gitRepoUrl, issue.notionUrl, issueNumber, prNumber);
   messageService.showCreatePRSuccess(updatedIssue);
+  if (localConfig.finish.checkOutToDevelop) {
+    await gitPlatformService.checkoutToBranchWithoutReset(
+      baseBranch ? baseBranch : localConfig.branchNames.develop
+    );
+  }
 };
