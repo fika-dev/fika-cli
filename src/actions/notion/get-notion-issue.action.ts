75e3ceef857b569496f6358095186bf432fc06db
Wonmo Jung <jungcro@gmail.com>
2022-07-29 17:22:47 +0900

    [rm] showGettingIssue from getNotionIssue

--- src/actions/notion/get-notion-issue.action.ts
+++ src/actions/notion/get-notion-issue.action.ts
@@ -7,10 +7,8 @@ import { IMessageService } from "@/domain/service/i_message.service";
 import { NotionUrl } from "@/domain/value_object/notion_url.vo";
 
 export const getNotionIssue = async (notionDocumentUrl: NotionUrl): Promise<Issue> => {
-  const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
   const connectService = container.get<IConnectService>(SERVICE_IDENTIFIER.ConnectService);
   const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
-  messageService.showGettingIssue();
   const botId = configService.getNotionBotId();
   const issue = await connectService.getIssue(notionDocumentUrl, botId);
   return issue;
