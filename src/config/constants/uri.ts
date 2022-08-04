4a96e78d65f17aa5377d3752f6c09315e716ff89
Wonmo Jung <jungcro@gmail.com>
2022-08-04 15:06:04 +0900

    [fix] production API to fixed one from urls file

--- src/config/constants/uri.ts
+++ src/config/constants/uri.ts
@@ -1,2 +1,3 @@
 export const notionAuthorizeUri: string = "https://api.notion.com/v1/oauth/authorize";
 export const fikaNotionClientId: string = "3b87a929-9ddd-4578-ad56-01243af38fb1";
+export const fikaApiUrl: string = "https://api.fikadev.com";
--- src/config/ioc_config.ts
+++ src/config/ioc_config.ts
@@ -1,7 +1,10 @@
-import "reflect-metadata";
-import { Container } from "inversify";
 import { ErrorHandlingService } from "@/domain/service/error_handling.service";
+import { IPromptService } from "@/domain/service/i-prompt.service";
 import { IErrorHandlingService } from "@/domain/service/i_error_handling.service";
+import { PromptService } from "@/domain/service/prompt.service";
+import dotenv from "dotenv";
+import { Container } from "inversify";
+import "reflect-metadata";
 import { IGitPlatformService } from "src/domain/entity/i_git_platform.service";
 import { AnalyzeService } from "src/domain/service/analyze.service";
 import { ConfigService } from "src/domain/service/config.service";
@@ -17,13 +20,10 @@ import { MessageService } from "src/domain/service/message.service";
 import { MorphService } from "src/domain/service/morph.service";
 import { SnapshotService } from "src/domain/service/snapshot.service";
 import SERVICE_IDENTIFIER, { PARAMETER_IDENTIFIER } from "./constants/identifiers";
-import { IPromptService } from "@/domain/service/i-prompt.service";
-import { PromptService } from "@/domain/service/prompt.service";
 import { FIKA_PATH } from "./constants/path";
-import dotenv from "dotenv";
+import { fikaApiUrl } from "./constants/uri";
 
 dotenv.config();
-
 let container = new Container();
 
 container
@@ -61,12 +61,12 @@ container
   .inSingletonScope();
 
 if (!process.env.FIKA_ENV) {
-  container.bind<string>(PARAMETER_IDENTIFIER.Domain).toConstantValue(process.env.API_ADDRESS);
+  container.bind<string>(PARAMETER_IDENTIFIER.Domain).toConstantValue(fikaApiUrl);
   const homePath = require("os").homedir();
   container.bind<string>(PARAMETER_IDENTIFIER.FikaPath).toConstantValue(`${homePath}/${FIKA_PATH}`);
   container.bind<string>(PARAMETER_IDENTIFIER.GitRepoPath).toConstantValue(process.cwd());
 } else if (process.env.FIKA_ENV === "production") {
-  container.bind<string>(PARAMETER_IDENTIFIER.Domain).toConstantValue(process.env.API_ADDRESS);
+  container.bind<string>(PARAMETER_IDENTIFIER.Domain).toConstantValue(fikaApiUrl);
   const homePath = require("os").homedir();
   container.bind<string>(PARAMETER_IDENTIFIER.FikaPath).toConstantValue(`${homePath}/${FIKA_PATH}`);
   container.bind<string>(PARAMETER_IDENTIFIER.GitRepoPath).toConstantValue(process.cwd());
