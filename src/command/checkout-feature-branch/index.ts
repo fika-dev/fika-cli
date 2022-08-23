1047e37055936c3311e1b08875a972d6d20608c4
tuxshido <c.metman@e.email>
2022-08-23 18:58:03 +0900

    [add] index for the checkout-feature-branch

--- src/command/checkout-feature-branch/index.ts
+++ src/command/checkout-feature-branch/index.ts
@@ -2,9 +2,9 @@ import { Command } from "commander";
 import { asyncWrapper, commandWrapper } from "../wrapper/wrappers";
 import { checkoutFeatureBranchAction } from "./checkout-feature-branch.action";
 
-export const finishCommand = new Command()
-  .command("finish")
-  .description("checking out to the most recent feature branch")
+export const checkoutFeatureBranchCommand = new Command()
+  .command("f")
+  .description("checking out to the most recent local feature branch")
   .action(async option => {
     commandWrapper(checkoutFeatureBranchAction);
   });
