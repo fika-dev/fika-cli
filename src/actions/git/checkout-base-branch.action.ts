2279406d548e389d1b773baed6fc9378cf759535
tuxshido <c.metman@e.email>
2022-08-23 10:05:37 +0900

    [Created] checkoutBaseBranch function to refactor the code

---
+++ src/actions/git/checkout-base-branch.action.ts
@@ -0,0 +1,13 @@
+import SERVICE_IDENTIFIER from "@/config/constants/identifiers";
+import container from "@/config/ioc_config";
+import { IGitPlatformService } from "@/domain/entity/i_git_platform.service";
+import { IMessageService } from "@/domain/service/i_message.service";
+
+export const checkoutBaseBranch = async (baseBranch: string): Promise<void> => {
+  const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
+  const gitPlatformService = container.get<IGitPlatformService>(
+    SERVICE_IDENTIFIER.GitPlatformService
+  );
+  await gitPlatformService.checkoutToBranchWithoutReset(baseBranch);
+  messageService.showSuccess(`Checkout to branch: ${baseBranch}`);
+};
