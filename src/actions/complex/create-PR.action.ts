3093537441e7f0f97c7fa82bd8a8b8efb28a3bbc
tuxshido <c.metman@e.email>
2022-08-26 10:09:05 +0900

    [refactor] with dart-import

--- src/actions/complex/create-PR.action.ts
+++ src/actions/complex/create-PR.action.ts
@@ -1,15 +1,12 @@
 import { Issue } from "@/domain/entity/issue.entity";
 import { IGitPlatformService } from "@/domain/entity/i_git_platform.service";
 import { IMessageService } from "@/domain/service/i_message.service";
-import { NotionUrl } from "@/domain/value_object/notion_url.vo";
 import SERVICE_IDENTIFIER from "src/config/constants/identifiers";
 import container from "src/config/ioc_config";
 import { IConfigService } from "src/domain/service/i_config.service";
 import { IConnectService } from "src/domain/service/i_connect.service";
 import { getFikaIssue } from "../fika/get-fika-issue.action";
-import { createGitPlatformIssue } from "../git/create-git-platform-issue.action";
 import { createGitPlatformPR } from "../git/create-git-platform-PR.action";
-import { getNotionIssue } from "../notion/get-notion-issue.action";
 
 export const createPR = async (): Promise<void> => {
   const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
