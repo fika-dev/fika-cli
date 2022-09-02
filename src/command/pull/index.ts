e071c3009cac6b966fcffba13f294f4ada9a706b
Wonmo Jung <jungcro@gmail.com>
2022-09-02 18:02:47 +0900

    [link] pullCommand to commander

--- src/command/pull/index.ts
+++ src/command/pull/index.ts
@@ -2,7 +2,7 @@ import { Command } from "commander";
 import { commandWrapper } from "../wrapper/wrappers";
 import { pullAction } from "./pull.action";
 
-export const initCommand = new Command()
+export const pullCommand = new Command()
   .command("pull")
   .description("pull from a codebase repo")
   .action(async () => {
