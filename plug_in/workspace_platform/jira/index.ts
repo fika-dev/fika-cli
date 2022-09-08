50c613fa8374f82086d94c43d07fb5dfebef0b1e
Wonmo Jung <jungcro@gmail.com>
2022-09-08 11:25:59 +0900

    [add]  test jira client id & offline scope

--- plug_in/workspace_platform/jira/index.ts
+++ plug_in/workspace_platform/jira/index.ts
@@ -1,8 +1,9 @@
 import { WorkspacePlatform } from "@/domain/entity/add_on/workspace_platform.entity";
 
 const jiraAuthorizeUri: string = "https://auth.atlassian.com/authorize";
-const fikaJraClientId: string = "4CtGOOySCpovViYXMVwRtPuPdlZtg9gA";
-const jiraSocpes: string[] = ["read:jira-work", "write:jira-work"];
+const fikaJiraClientId: string = "4CtGOOySCpovViYXMVwRtPuPdlZtg9gA";
+const testFikaJiraClientId: string = "MmjvsYfSW4xFEiZmr9UBPRdeX2TjenIC";
+const jiraSocpes: string[] = ["read:jira-work", "write:jira-work", "offline_access"];
 
 export class JiraWorkspace extends WorkspacePlatform {
   constructor() {
@@ -11,10 +12,16 @@ export class JiraWorkspace extends WorkspacePlatform {
   }
 
   getAuthenticationUri(domain: string): string {
+    let clientId: string;
+    if (domain === process.env.TEST_API_ADDRESS) {
+      clientId = testFikaJiraClientId;
+    } else {
+      clientId = fikaJiraClientId;
+    }
     const redirectUri = `${domain}/workspace/${this.workspaceType}/callback`;
     const scopeParams = jiraSocpes.join("+");
 
-    const params = `audience=api.atlassian.com&client_id=${fikaJraClientId}&scope=${scopeParams}&redirect_uri=${redirectUri}&state=\${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent`;
+    const params = `audience=api.atlassian.com&client_id=${fikaJiraClientId}&scope=${scopeParams}&redirect_uri=${redirectUri}&state=\${YOUR_USER_BOUND_VALUE}&response_type=code&prompt=consent`;
     const targetUri = `${jiraAuthorizeUri}?${params}`;
     return targetUri;
   }
