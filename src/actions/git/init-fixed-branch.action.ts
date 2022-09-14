9dd27381b1e9b55114f3acd4932fefb833c205c2
Wonmo Jung <jungcro@gmail.com>
2022-09-14 13:20:23 +0900

    [fix] to check existence of remote branch

--- src/actions/git/init-fixed-branch.action.ts
+++ src/actions/git/init-fixed-branch.action.ts
@@ -1,12 +1,15 @@
 import SERVICE_IDENTIFIER from "@/config/constants/identifiers";
 import container from "@/config/ioc_config";
 import { IGitPlatformService } from "@/domain/entity/i_git_platform.service";
-import { IPromptService } from "@/domain/service/i-prompt.service";
 
 export const initFixedBranch = async (branchName: string): Promise<void> => {
   const gitPlatformService = container.get<IGitPlatformService>(
     SERVICE_IDENTIFIER.GitPlatformService
   );
   await gitPlatformService.checkoutToBranchWithoutReset(branchName);
-  await gitPlatformService.pushBranchWithUpstream(branchName);
+  await gitPlatformService.fetchFromRemote();
+  const isRemoteBranchExist = await gitPlatformService.checkRemoteBranchExist(branchName);
+  if (!isRemoteBranchExist) {
+    await gitPlatformService.pushBranchWithUpstream(branchName);
+  }
 };
