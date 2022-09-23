ffbd5ceb01fb587e1b79778a0bca77d8def88f6f
Wonmo Jung <jungcro@gmail.com>
2022-09-23 09:50:12 +0900

    [apply] TE

--- src/infrastructure/services/implementation/commander.service.ts
+++ src/infrastructure/services/implementation/commander.service.ts
@@ -1,43 +1,49 @@
 import { PARAMETER_IDENTIFIER } from "@/config/constants/identifiers";
-import {
-  GitCommand,
-  GitCommandError,
-  GitCommandExecuter,
-  GitCommandResult,
-  Success,
-} from "@/domain/git-command/command.types";
+import { GitCommandError, GitCommandExecuter } from "@/domain/git-command/command.types";
 import { exec } from "child_process";
+import { pipe } from "fp-ts/lib/function";
+import * as TE from "fp-ts/TaskEither";
 import { inject } from "inversify";
 import { promisify } from "util";
-import { ICmdService } from "../interface/commander.service";
-import * as E from "fp-ts/Either";
+import { ICmdService } from "../interface/i_commander.service";
 class GitCommanderServie implements ICmdService {
   private _gitRepoPath: string;
   constructor(@inject(PARAMETER_IDENTIFIER.GitRepoPath) gitRepoPath: string) {
     this._gitRepoPath = gitRepoPath;
   }
 
-  public excuteGitCommand: GitCommandExecuter = async gitCommand => {
-    try {
-      let command: string;
-      if (process.platform == "win32") {
-        const windowsCommand = gitCommand.windowsCommand ?? gitCommand.command;
-        command = `git ${windowsCommand}`;
-      } else {
-        command = `LC_ALL=C git ${gitCommand.command}`;
-      }
-      const result = await this.exec(command);
-      return E.right({
-        output: result.stdout + result.stderr,
-      }) as GitCommandResult;
-    } catch (e) {
-      const message = e.stdout + e.stderr;
-      return E.left(message) as GitCommandResult;
-    }
+  public excuteGitCommand: GitCommandExecuter = gitCommand => {
+    return pipe(
+      TE.tryCatch(
+        () => {
+          let command: string;
+          if (process.platform == "win32") {
+            const windowsCommand = gitCommand.windowsCommand ?? gitCommand.command;
+            command = `git ${windowsCommand}`;
+          } else {
+            command = `LC_ALL=C git ${gitCommand.command}`;
+          }
+          return this.exec(command);
+        },
+        (e: any) => {
+          return { message: e };
+        }
+      ),
+      TE.map(result => {
+        return {
+          output: result.stdout + result.stderr,
+        };
+      })
+    );
   };
 
   private async exec(command: string) {
-    const execP = promisify(exec);
-    return await execP(command, { cwd: this._gitRepoPath });
+    try {
+      const execP = promisify(exec);
+      return await execP(command, { cwd: this._gitRepoPath });
+    } catch (e) {
+      const message = e.stdout + e.stderr;
+      return message;
+    }
   }
 }
