7af1c000a6d9e6e311e97d129ae29c478a295cc8
Wonmo Jung <jungcro@gmail.com>
2022-09-23 09:52:27 +0900

    [add] [exp] functional implementation

--- src/command/checkout-feature-branch/checkout-feature-branch.action.ts
+++ src/command/checkout-feature-branch/checkout-feature-branch.action.ts
@@ -1,11 +1,26 @@
-import { IPromptService } from "@/domain/service/i-prompt.service";
-import { IMessageService } from "@/domain/service/i_message.service";
+import * as E from "fp-ts/Either";
+import { flow } from "fp-ts/lib/function";
+import * as O from "fp-ts/Option";
+import * as T from "fp-ts/Task";
+import * as TE from "fp-ts/TaskEither";
 import SERVICE_IDENTIFIER from "src/config/constants/identifiers";
 import container from "src/config/ioc_config";
-import { IGitPlatformService } from "@/domain/service/i_git_platform.service";
-import { IConfigService } from "@/domain/service/i_config.service";
+import { IPromptService } from "src/domain/service/i-prompt.service";
+import { IConfigService } from "src/domain/service/i_config.service";
+import { IGitPlatformService } from "src/domain/service/i_git_platform.service";
+import { IMessageService } from "src/domain/service/i_message.service";
 
-export const checkoutFeatureBranchAction = async (issueNumber?: number) => {
+import { GetContext } from "@/domain/context/context.types";
+import { Unit } from "@/domain/general/general.types";
+import { CheckoutToIssue, GitCommandExecuter } from "@/domain/git-command/command.types";
+import { GetIssueRecordByNumber, ValidateIssueNumber } from "@/domain/issue/issue.types";
+import { ReportError, ReportSuccess } from "@/domain/report/report.types";
+import { ValidateContext } from "@/domain/rules/validation-rules/validation-rule.types";
+import { RuleCombinator } from "src/domain/rules/rule.types";
+import { Context } from "vm";
+import { checkoutToIssueBuilder } from "@/domain/git-command/command.functions";
+
+const _checkoutFeatureBranchLegacy = async (issueNumber?: number) => {
   const promptService = container.get<IPromptService>(SERVICE_IDENTIFIER.PromptService);
   const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
   const gitPlatformService = container.get<IGitPlatformService>(
@@ -13,6 +28,7 @@ export const checkoutFeatureBranchAction = async (issueNumber?: number) => {
   );
   const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
   const isChangeExist = await gitPlatformService.checkUnstagedChanges();
+
   let featureBranch: string;
   if (issueNumber && !isNaN(issueNumber)) {
     featureBranch = configService.getIssueBranch(issueNumber);
@@ -35,3 +51,37 @@ export const checkoutFeatureBranchAction = async (issueNumber?: number) => {
     messageService.showWarning("Could not find a feature branch that matches your request");
   }
 };
+export const checkoutFeatureBranchAction = _checkoutFeatureBranchLegacy;
+
+const _functionalCheckoutFeatureBranch = async (issueNumber?: number) => {
+  const validateCheckOut = validateCheckOutBuilder(getContext)(setContext);
+  const checkoutToIssue = checkoutToIssueBuilder(exec);
+  return flow(
+    O.fromNullable,
+    O.fold(
+      () => T.of("Unit"),
+      flow(
+        validateIssueNumber,
+        E.chainFirst(() => validateCheckOut(isCheckOutOkRule)),
+        TE.fromEither,
+        TE.chain(findIssueRecordByNumber),
+        TE.chain(checkoutToIssue),
+        TE.fold(
+          e => T.of(reportError(e)),
+          s => T.of(reportSuccess(s))
+        )
+      )
+    ),
+    O.getOrElse
+  )(issueNumber);
+};
+
+declare const getContext: GetContext;
+declare function setContext(context: Context): Unit;
+declare const validateCheckOutBuilder: ValidateContext;
+declare const exec: GitCommandExecuter;
+declare const validateIssueNumber: ValidateIssueNumber;
+declare const findIssueRecordByNumber: GetIssueRecordByNumber;
+declare const reportError: ReportError;
+declare const reportSuccess: ReportSuccess;
+declare const isCheckOutOkRule: RuleCombinator;
