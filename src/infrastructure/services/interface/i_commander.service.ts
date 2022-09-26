9a69144f9dbb60371f2c76075c1b6312b18cc821
tuxshido <c.metman@e.email>
2022-09-26 15:45:20 +0900

    [add] command context functions

--- src/infrastructure/services/interface/i_commander.service.ts
+++ src/infrastructure/services/interface/i_commander.service.ts
@@ -1,5 +1,6 @@
-import { ExecuteGitCommand } from "@/domain/git-command/command.types";
+import { ExecuteCommand, ExecuteGitCommand } from "@/domain/git-command/command.types";
 
 export interface ICmdService {
   excuteGitCommand: ExecuteGitCommand;
+  excuteCommand: ExecuteCommand;
 }
