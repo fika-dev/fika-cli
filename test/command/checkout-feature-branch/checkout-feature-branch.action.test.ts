e4eeb1b1e790a4e98b7b1d45b8aa0e75a41e5de0
Wonmo Jung <jungcro@gmail.com>
2022-09-30 11:11:14 +0900

    add: tests

--- test/command/checkout-feature-branch/checkout-feature-branch.action.test.ts
+++ test/command/checkout-feature-branch/checkout-feature-branch.action.test.ts
@@ -1,14 +1,16 @@
 import { checkoutFeatureBranchAction } from "@/command/checkout-feature-branch/checkout-feature-branch.action";
 import SERVICE_IDENTIFIER from "@/config/constants/identifiers";
 import container from "@/config/ioc_config";
-import { IGitPlatformService } from "@/domain/service/i_git_platform.service";
 import { IPromptService } from "@/domain/service/i-prompt.service";
 import { IConfigService } from "@/domain/service/i_config.service";
-import { IErrorHandlingService } from "@/domain/service/i_error_handling.service";
+import { IGitPlatformService } from "@/domain/service/i_git_platform.service";
 import { IMessageService } from "@/domain/service/i_message.service";
 import { Uuid } from "@/domain/value_object/uuid.vo";
-import { TEST_CHANGE_FILE_PATH, TEST_CPR_BRANCH_NAME } from "test/test-constants";
-import { checkAndCloneRepo, createTestConfig, makeMeaninglessChange, restoreGitRepo, setUseToken, stageAndCommit } from "test/test-utils";
+import { ICommanderService } from "@/infrastructure/services/interface/i_commander.service";
+import { checkAndCloneRepo, createTestConfig, restoreGitRepo, setUseToken } from "test/test-utils";
+import * as T from 'fp-ts/Task';
+import { TEST_BRANCH_LIST, TEST_CPR_BRANCH_NAME, TEST_GIT_CLEAN_STATUS, TEST_HTTPS_GITHUB_REPO } from "test/test-constants";
+import { ExecuteGitCommand } from "@/domain/git-command/command.types";
 
 const gitPlatformService = container.get<IGitPlatformService>(SERVICE_IDENTIFIER.GitPlatformService);
 const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
@@ -23,8 +25,6 @@ beforeAll(async () => {
 
 beforeEach(async()=>{
   jest.restoreAllMocks();
-//   jest.spyOn(process.stdout, "write").mockImplementation(()=>true);
-//   jest.spyOn(console, "log").mockImplementation((m)=>console.log(m));
   jest.spyOn(messageService, 'showSuccess').mockImplementation(()=>{});
   jest.spyOn(configService, 'getWorkspaceId').mockImplementation(()=>new Uuid('d3224eba-6e67-4730-9b6f-a9ef1dc7e4ac'));
   await gitPlatformService.checkoutToBranchWithoutReset('develop');
@@ -40,59 +40,50 @@ afterAll(() => {
   
 });
 
-it("1.test if checkoutFeatureBranch go to the latest feature branch", async () => {
-    await gitPlatformService.checkoutToBranchWithoutReset(TEST_CPR_BRANCH_NAME);
-    makeMeaninglessChange(TEST_CHANGE_FILE_PATH);
-    await stageAndCommit('Just commit a meaningless change');
-    await gitPlatformService.checkoutToBranchWithoutReset('develop');
-    await checkoutFeatureBranchAction();
-    const currentBranch = await gitPlatformService.getBranchName();
-    expect(currentBranch).toBe(TEST_CPR_BRANCH_NAME);
-    await gitPlatformService.checkoutToBranchWithoutReset(TEST_CPR_BRANCH_NAME);
-    await gitPlatformService.undoCommitAndModification();
-});
 
-it("2.test when no feature branch if getLatestBranchByCommitDate return undefined", async () => {
-    jest.spyOn(gitPlatformService,'getSortedBranchesByCommitDate').mockImplementation(()=>Promise.resolve([]));
-    const latesBranch = await gitPlatformService.getLatestBranchByCommitDate();
-    expect(latesBranch).toBeUndefined();
-});
- 
-it("3.test when no feature branch if checkoutFeaturebranch stay in ths same branch and send the correct warning", async () => {
-    const beforeBranch = await gitPlatformService.getBranchName();
-    jest.spyOn(gitPlatformService,'getLatestBranchByCommitDate').mockImplementation(()=>Promise.resolve(undefined));
-    const spy = jest.spyOn(messageService, 'showWarning').mockImplementation(() => { });
-     await checkoutFeatureBranchAction();
-    const afterBranch = await gitPlatformService.getBranchName();
-    expect(beforeBranch).toEqual(afterBranch);
-    expect(spy).toBeCalledWith("Could not find a feature branch that matches your request");
-});
- 
-it("4.test when the branch number is a number", async () => {
-const beforeBranch = await gitPlatformService.checkoutToBranchWithoutReset('develop');;
-  await checkoutFeatureBranchAction(406);
-  const afterBranch = await gitPlatformService.getBranchName();
-  expect(afterBranch).toEqual("feature/iss/#406")
-})
-
-it('5.test when the branch is NOT a number', async () => {
-  const spy = jest.spyOn(messageService, 'showWarning').mockImplementation(() => { });
-  await checkoutFeatureBranchAction('AR406' as any)
-  expect(spy).toBeCalledWith("Could not understand your request, please provide a valid number");
-   
-})
-
-it("6.test when the branch number is a number but the issue number doesn't exist", async () => {
-  const beforeBranch = await gitPlatformService.checkoutToBranchWithoutReset('develop');
-  const errorService = container.get<IErrorHandlingService>(
-    SERVICE_IDENTIFIER.ErrorHandlingService
-    );
-   const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
-  const spy = jest.spyOn(messageService, 'showError').mockImplementation((e) => e.message);
-  try {
-    await checkoutFeatureBranchAction(4067765);
-    expect(spy).toBeCalledWith('No remote branch was found');
-  } catch (e) {
-    errorService.handle(e);
+const mockExecuteGitCommand: ExecuteGitCommand = (gitCommand) => {
+  if (gitCommand.command === "branch --sort=-committerdate --format='%(refname:short)'"){
+    return T.of(TEST_CPR_BRANCH_NAME);
   }
-})
+  else if (gitCommand.command === 'status'){
+    return T.of(TEST_GIT_CLEAN_STATUS);
+  }else if (gitCommand.command === 'rev-parse --abbrev-ref HEAD'){
+    return T.of('Somthing strange is here');
+  }else if (gitCommand.command === 'remote get-url'){
+    return T.of(TEST_HTTPS_GITHUB_REPO);
+  }else{
+    return T.of(TEST_BRANCH_LIST);
+  }
+}
+
+
+ 
+ 
+// it("4.test when the branch number is a number", async () => {
+// const beforeBranch = await gitPlatformService.checkoutToBranchWithoutReset('develop');;
+//   await checkoutFeatureBranchAction(406);
+//   const afterBranch = await gitPlatformService.getBranchName();
+//   expect(afterBranch).toEqual("feature/iss/#406")
+// })
+
+// it('5.test when the branch is NOT a number', async () => {
+//   const spy = jest.spyOn(messageService, 'showWarning').mockImplementation(() => { });
+//   await checkoutFeatureBranchAction('AR406' as any)
+//   expect(spy).toBeCalledWith("Could not understand your request, please provide a valid number");
+   
+// })
+
+// it("6.test when the branch number is a number but the issue number doesn't exist", async () => {
+//   const beforeBranch = await gitPlatformService.checkoutToBranchWithoutReset('develop');
+//   const errorService = container.get<IErrorHandlingService>(
+//     SERVICE_IDENTIFIER.ErrorHandlingService
+//     );
+//    const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
+//   const spy = jest.spyOn(messageService, 'showError').mockImplementation((e) => e.message);
+//   try {
+//     await checkoutFeatureBranchAction(4067765);
+//     expect(spy).toBeCalledWith('No remote branch was found');
+//   } catch (e) {
+//     errorService.handle(e);
+//   }
+// })
---
+++ test/domain/git-command/command.functions.test.ts
@@ -0,0 +1,287 @@
+import SERVICE_IDENTIFIER from "@/config/constants/identifiers";
+import { checkoutToIssue, checkoutWithChanges, getLatestBranchByCommit, getRemoteOrigin } from "@/domain/git-command/command.functions";
+import { ExecuteGitCommand } from "@/domain/git-command/command.types";
+import { applyStashCmd, createBranchCmd, fetchCmd, getBranchesCmd, getRemoteBranchesCmd, getRemoteUrlCmd, stashCmd } from "@/domain/git-command/git-command.values";
+import { IPromptService } from "@/domain/service/i-prompt.service";
+import * as T from 'fp-ts/Task';
+import container from "src/config/ioc_config";
+import { TEST_BRANCH_SORTED, TEST_CPR_BRANCH_NAME, TEST_GIT_CLEAN_STATUS, TEST_GIT_NO_REMOTE, TEST_GIT_STASH_APPLY_ERR, TEST_GIT_STASH_APPLY_NORMAL_OUTPUT, TEST_GIT_STATUS_WITH_STAGED, TEST_HTTPS_GITHUB_REPO, TEST_ISSUE_BRANCH_TEMPLATE, TEST_NOT_LOCAL_BRANCH, TEST_NOT_LOCAL_BUT_REMOTE_BRANCH, TEST_REMOTE_BRANCHES, TEST_SSH_GITHUB_REPO } from "test/test-constants";
+
+beforeAll(()=>{
+  jest.spyOn(process.stdout, "write").mockImplementation(()=>true);
+});
+
+beforeEach(()=>{
+  container.snapshot();
+});
+
+afterEach(()=>{
+  container.restore();
+});
+
+
+
+test('1. getLatestBranchByCommit', async () => {
+  const mockExecuteGitCommand: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === "branch --format='%(refname:short)'"){
+      return T.of(TEST_BRANCH_SORTED);
+    }
+  }
+  const mockExecuteGitCommandForError: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === "branch --format='%(refname:short)'"){
+      return T.of('develop');
+    }
+  }
+  const branch =  await getLatestBranchByCommit(mockExecuteGitCommand)(TEST_ISSUE_BRANCH_TEMPLATE);
+  expect(branch).toBe(TEST_CPR_BRANCH_NAME);
+  try{
+    const developBranch =  await getLatestBranchByCommit(mockExecuteGitCommandForError)(TEST_ISSUE_BRANCH_TEMPLATE);
+  }catch(e){
+    expect(e).toEqual({
+      type: "NoFeatureBranchInLocal",
+    });
+  }
+});
+
+
+test('2.1 checkoutWithChanges without stash', async () => {
+  let checkoutBranch: string;
+  const mockExecuteGitCommandClean: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === "status"){
+      return T.of(TEST_GIT_CLEAN_STATUS);
+    }
+    if (gitCommand.command === "checkout"){
+      checkoutBranch = gitCommand.argument;
+      return T.of('');
+    }
+  }
+  await checkoutWithChanges(mockExecuteGitCommandClean)(TEST_CPR_BRANCH_NAME);
+  expect(checkoutBranch).toBe(TEST_CPR_BRANCH_NAME);
+});
+
+test('2.2 checkoutWithChanges with stash', async () => {
+  const promptService = container.get<IPromptService>(SERVICE_IDENTIFIER.PromptService);
+  let checkoutBranch: string;
+  let stashHasbeenCalled = false;
+  let stashApplyHasbeenCalled = false;
+  jest.spyOn(promptService, 'confirmAction').mockImplementationOnce(()=>Promise.resolve(true));
+  const mockExecuteWithChange: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === "status"){
+      return T.of(TEST_GIT_STATUS_WITH_STAGED);
+    }
+    if (gitCommand.command === stashCmd.command){
+      stashHasbeenCalled = true;
+      return T.of(TEST_BRANCH_SORTED);
+    }
+    if (gitCommand.command === applyStashCmd.command){
+      stashApplyHasbeenCalled = true;
+      return T.of(TEST_GIT_STASH_APPLY_NORMAL_OUTPUT);
+    }
+    if (gitCommand.command === "checkout"){
+      checkoutBranch = gitCommand.argument;
+      return T.of('');
+    }
+  }
+  await checkoutWithChanges(mockExecuteWithChange)(TEST_CPR_BRANCH_NAME);
+  expect(stashHasbeenCalled).toBe(true);
+  expect(stashApplyHasbeenCalled).toBe(true);
+  expect(checkoutBranch).toBe(TEST_CPR_BRANCH_NAME);
+});
+
+
+test('2.3 checkoutWithChanges error while stash', async () => {
+  // [TODO] handle this case
+  const promptService = container.get<IPromptService>(SERVICE_IDENTIFIER.PromptService);
+  let checkoutBranch: string;
+  let stashHasbeenCalled = false;
+  let stashApplyHasbeenCalled = false;
+  jest.spyOn(promptService, 'confirmAction').mockImplementationOnce(()=>Promise.resolve(true));
+  const mockExecuteWithChange: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === "status"){
+      return T.of(TEST_GIT_STATUS_WITH_STAGED);
+    }
+    if (gitCommand.command === stashCmd.command){
+      stashHasbeenCalled = true;
+      return T.of(TEST_BRANCH_SORTED);
+    }
+    if (gitCommand.command === applyStashCmd.command){
+      stashApplyHasbeenCalled = true;
+      return T.of(TEST_GIT_STASH_APPLY_ERR);
+    }
+    if (gitCommand.command === "checkout"){
+      checkoutBranch = gitCommand.argument;
+      return T.of('');
+    }
+  }
+  await checkoutWithChanges(mockExecuteWithChange)(TEST_CPR_BRANCH_NAME);
+  expect(stashHasbeenCalled).toBe(true);
+  expect(stashApplyHasbeenCalled).toBe(true);
+  expect(checkoutBranch).toBe(TEST_CPR_BRANCH_NAME);
+});
+
+
+
+test('2.4 checkoutWithChanges refuse to move changes', async () => {
+  const promptService = container.get<IPromptService>(SERVICE_IDENTIFIER.PromptService);
+  let checkoutBranch: string;
+  let stashHasbeenCalled = false;
+  let stashApplyHasbeenCalled = false;
+  jest.spyOn(promptService, 'confirmAction').mockImplementationOnce(()=>Promise.resolve(false));
+  const mockExecuteWithChange: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === "status"){
+      return T.of(TEST_GIT_STATUS_WITH_STAGED);
+    }
+    if (gitCommand.command === stashCmd.command){
+      stashHasbeenCalled = true;
+      return T.of(TEST_BRANCH_SORTED);
+    }
+    if (gitCommand.command === applyStashCmd.command){
+      stashApplyHasbeenCalled = true;
+      return T.of(TEST_GIT_STASH_APPLY_ERR);
+    }
+    if (gitCommand.command === "checkout"){
+      checkoutBranch = gitCommand.argument;
+      return T.of('');
+    }
+  }
+  try{
+    await checkoutWithChanges(mockExecuteWithChange)(TEST_CPR_BRANCH_NAME);
+    expect(false).toBe(true);
+  }catch(e){
+    expect(stashHasbeenCalled).toBe(false);
+    expect(stashApplyHasbeenCalled).toBe(false);
+    expect(checkoutBranch).toBe(undefined);
+  }
+});
+
+
+test('3.1 getRemoteOrigin with url', async () => {
+  const mockExecuteWithChange: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === getRemoteUrlCmd.command){
+      return T.of(TEST_HTTPS_GITHUB_REPO);
+    }
+  }
+  const remoteOrigin = await getRemoteOrigin(mockExecuteWithChange)();
+  expect(remoteOrigin).toBe(TEST_HTTPS_GITHUB_REPO);
+});
+
+test('3.2 getRemoteOrigin with empty', async () => {
+  const mockExecuteWithChange: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === getRemoteUrlCmd.command){
+      return T.of(TEST_GIT_NO_REMOTE);
+    }
+  }
+  const remoteOrigin = await getRemoteOrigin(mockExecuteWithChange)();
+  expect(remoteOrigin).toBe("Empty");
+});
+
+test('4.1 checkoutToIssue without creating branch', async () => {
+  let checkoutBranch: string;
+  let hasCreateBranchCalled = false;
+  const mockExecuteWithChange: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === "status"){
+      return T.of(TEST_GIT_CLEAN_STATUS);
+    }
+    if (gitCommand.command === createBranchCmd.command){
+      hasCreateBranchCalled = true;
+      return T.of('');
+    }
+    if (gitCommand.command === "checkout"){
+      checkoutBranch = gitCommand.argument;
+      return T.of('');
+    }
+    if (gitCommand.command === getBranchesCmd.command){
+      return T.of(TEST_BRANCH_SORTED)
+    }
+    if (gitCommand.command === getRemoteBranchesCmd.command){
+      return T.of(TEST_REMOTE_BRANCHES)
+    }
+  }
+  await checkoutToIssue(mockExecuteWithChange)({
+    issueUrl: '',
+    title: '',
+    labels: [],
+    branchName: TEST_CPR_BRANCH_NAME,
+  });
+  expect(checkoutBranch).toBe(TEST_CPR_BRANCH_NAME);
+  expect(hasCreateBranchCalled).toBe(false);
+});
+
+test('4.2 checkoutToIssue with creating local tracking branch', async () => {
+  let checkoutBranch: string;
+  let hasCreateBranchCalled = false;
+  let gitFetchHasbeenCalled = false;
+  const mockExecuteWithChange: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === "status"){
+      return T.of(TEST_GIT_CLEAN_STATUS);
+    }
+    if (gitCommand.command === createBranchCmd.command){
+      hasCreateBranchCalled = true;
+      return T.of('');
+    }
+    if (gitCommand.command === "checkout"){
+      checkoutBranch = gitCommand.argument;
+      return T.of('');
+    }
+    if (gitCommand.command === getBranchesCmd.command){
+      return T.of(TEST_BRANCH_SORTED)
+    }
+    if (gitCommand.command === getRemoteBranchesCmd.command){
+      return T.of(TEST_REMOTE_BRANCHES)
+    }
+    if (gitCommand.command === fetchCmd.command){
+      gitFetchHasbeenCalled = true;
+      return T.of('');
+    }
+  }
+  await checkoutToIssue(mockExecuteWithChange)({
+    issueUrl: '',
+    title: '',
+    labels: [],
+    branchName: TEST_NOT_LOCAL_BUT_REMOTE_BRANCH,
+  });
+  expect(checkoutBranch).toBe(TEST_NOT_LOCAL_BUT_REMOTE_BRANCH);
+  expect(gitFetchHasbeenCalled).toBe(true);
+  expect(hasCreateBranchCalled).toBe(true);
+});
+
+test('4.3 checkoutToIssue with no local and remote error', async () => {
+  let checkoutBranch: string;
+  let hasCreateBranchCalled = false;
+  const mockExecuteWithChange: ExecuteGitCommand = (gitCommand) => {
+    if (gitCommand.command === "status"){
+      return T.of(TEST_GIT_CLEAN_STATUS);
+    }
+    if (gitCommand.command === createBranchCmd.command){
+      hasCreateBranchCalled = true;
+      return T.of('');
+    }
+    if (gitCommand.command === "checkout"){
+      checkoutBranch = gitCommand.argument;
+      return T.of('');
+    }
+    if (gitCommand.command === getBranchesCmd.command){
+      return T.of(TEST_BRANCH_SORTED)
+    }
+    if (gitCommand.command === getRemoteBranchesCmd.command){
+      return T.of(TEST_REMOTE_BRANCHES)
+    }
+    if (gitCommand.command === fetchCmd.command){
+      return T.of('');
+    }
+  }
+  try{
+    await checkoutToIssue(mockExecuteWithChange)({
+      issueUrl: '',
+      title: '',
+      labels: [],
+      branchName: TEST_NOT_LOCAL_BRANCH,
+    });
+  }catch(e){
+    console.log('🧪', ' in CommandFunctionsTest: ', 'e: ',e);
+    expect(e.type).toBe("NoLocalAndRemoteBranch")
+    expect(hasCreateBranchCalled).toBe(false);
+  }
+});
+
+
