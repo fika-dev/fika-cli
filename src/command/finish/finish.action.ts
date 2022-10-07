8edad07a3d48501e78be5dcab64fdc77692d5ac0
Wonmo Jung <jungcro@gmail.com>
2022-10-07 17:13:35 +0900

    fix

--- src/command/finish/finish.action.ts
+++ src/command/finish/finish.action.ts
@@ -1,17 +1,13 @@
+import { checkoutDependingOnConfig } from "@/actions/complex/checkout-depending-on-config.action";
 import { createPR } from "@/actions/complex/create-PR.action";
 import { askToContinueWithUncommitedChanges } from "@/actions/git/ask-to-continue-with-uncommited-changes.action";
-import { checkoutBaseBranch } from "@/actions/git/checkout-base-branch.action";
 import { gitPullAction } from "@/actions/git/git-pull.action";
-import { IPromptService } from "@/domain/service/i-prompt.service";
-import { IMessageService } from "@/domain/service/i_message.service";
 import SERVICE_IDENTIFIER from "src/config/constants/identifiers";
 import container from "src/config/ioc_config";
 import { IConfigService } from "src/domain/service/i_config.service";
 
 export const finishAction = async (baseBranch?: string) => {
   const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
-  const promptService = container.get<IPromptService>(SERVICE_IDENTIFIER.PromptService);
-  const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
   const localConfig = configService.getLocalConfig();
   const developBranch = baseBranch ? baseBranch : localConfig.branchNames.develop;
   await askToContinueWithUncommitedChanges();
@@ -19,16 +15,5 @@ export const finishAction = async (baseBranch?: string) => {
     await gitPullAction(developBranch);
   }
   await createPR();
-  if (localConfig.finish.checkOutToDevelop) {
-    await checkoutBaseBranch(developBranch);
-  } else {
-    const proceed = await promptService.confirmAction(
-      "Do you wanna stay in your current feature branch? (y or n)"
-    );
-    if (proceed === true) {
-      messageService.showSuccess(`Staying in the current branch`);
-    } else {
-      await checkoutBaseBranch(developBranch);
-    }
-  }
+  await checkoutDependingOnConfig(localConfig)(developBranch);
 };
--- test/command/finish/finish.action.test.ts
+++ test/command/finish/finish.action.test.ts
@@ -1,45 +1,91 @@
-import { createPR } from "@/actions/complex/create-PR.action";
-import { finishAction } from "@/command/finish/finish.action";
-import { defaultLocalConfig } from "@/config/constants/default_config";
 import SERVICE_IDENTIFIER from "@/config/constants/identifiers";
 import container from "@/config/ioc_config";
-import { IGitPlatformService } from "@/domain/service/i_git_platform.service";
+import { GitCommand } from "@/domain/git-command/command.types";
+import { checkoutCmd, createBranchCmd, fetchCmd, getBranchesCmd, getCurrentBranchCmd, getRemoteBranchesCmd, getRemoteOriginCmd, pullFromCmd, statusCmd } from "@/domain/git-command/git-command.values";
 import { IPromptService } from "@/domain/service/i-prompt.service";
 import { IConfigService } from "@/domain/service/i_config.service";
+import { IGitPlatformService } from "@/domain/service/i_git_platform.service";
 import { IMessageService } from "@/domain/service/i_message.service";
-import BaseException from "@/domain/value_object/exceptions/base_exception";
-import { GhPrAlreadyExists } from "@/domain/value_object/exceptions/gh_pr_already_exists";
+import { TEST_BRANCH_LIST, TEST_CPR_DOC_ID, TEST_GIT_CLEAN_STATUS, TEST_GIT_PULL_CONFLICT_OUTPUT, TEST_GIT_PULL_UPDATED_OUTPUT, TEST_GIT_STATUS_WITH_STAGED, TEST_HTTPS_GITHUB_REPO, TEST_REMOTE_BRANCHES } from "test/test-constants";
+import { checkAndCloneRepo, createTestConfig, setUseToken, spyWithMock } from "test/test-utils";
+import * as T from "fp-ts/Task";
+import { finishAction } from "@/command/finish/finish.action";
+import { IConnectService } from "@/domain/service/i_connect.service";
 import { Uuid } from "@/domain/value_object/uuid.vo";
-import exp from "constants";
-import { TEST_CPR_BRANCH_NAME } from "test/test-constants";
-import { checkAndCloneRepo, createTestConfig, deleteBranch, restoreGitRepo, setUseToken } from "test/test-utils";
+import { defaultLocalConfig } from "@/config/constants/default_config";
+import { json } from "stream/consumers";
 
 const gitPlatformService = container.get<IGitPlatformService>(SERVICE_IDENTIFIER.GitPlatformService);
 const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
 const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
 const promptService = container.get<IPromptService>(SERVICE_IDENTIFIER.PromptService);
+const connectService = container.get<IConnectService>(SERVICE_IDENTIFIER.ConnectService);
+
+const defaultMock = (additionalMock)=> (cmd: GitCommand) => {
+  const t = additionalMock(cmd);
+  if (t !== undefined){
+    return t
+  }else{
+    if(cmd.command === pullFromCmd.command){
+      return T.of(TEST_GIT_PULL_UPDATED_OUTPUT);
+    }if (cmd.command === getCurrentBranchCmd.command){
+      return T.of('feature/iss/#2');
+    }if (cmd.command === statusCmd.command){
+      return T.of(TEST_GIT_CLEAN_STATUS);
+    }if (cmd.command === checkoutCmd.command){
+      return T.of('');
+    }if (cmd.command === createBranchCmd.command){
+      return T.of('');
+    }if (cmd.command === getBranchesCmd.command){
+      return T.of(TEST_BRANCH_LIST);
+    }if (cmd.command === getRemoteBranchesCmd.command){
+      return T.of(TEST_REMOTE_BRANCHES);
+    }if (cmd.command === fetchCmd.command){
+      return T.of('');
+    }if (cmd.command === getRemoteOriginCmd.command){
+      return T.of(TEST_HTTPS_GITHUB_REPO);
+    }
+    throw cmd;
+  }
+}
+
 
 beforeAll(async () => {
   await checkAndCloneRepo();
   createTestConfig(process.env.TESTING_PATH + "/.fika");
   setUseToken(process.env.TESTING_USER_TOKEN);
+  jest.restoreAllMocks();
+  jest.spyOn(messageService, 'showSuccess').mockImplementation(()=>{});
+  jest.spyOn(configService, 'getWorkspaceId').mockImplementation(()=>new Uuid('d3224eba-6e67-4730-9b6f-a9ef1dc7e4ac'));
+  jest.spyOn(configService, 'getWorkspaceType').mockImplementation(()=>'notion');
+  jest.spyOn(connectService, 'getIssueRecord').mockImplementation((issueNumber, gitRepoUrl)=>{
+    if (issueNumber === 2 && (gitRepoUrl == TEST_HTTPS_GITHUB_REPO.slice(0, -4))){
+      return Promise.resolve({
+        id: 'a38a4d5d-1407-4fce-a7ec-192d8ab58495',
+        title: 'test fika start doc',
+        issueUrl: 'https://www.notion.so/test-fika-start-doc-4af459df4efb448483fe3e2b703d50fd',
+        gitIssueUrl: 'https://github.com/fika-dev/fika-cli-test-samples/issues/2',
+        labels: [],
+        branchName: 'feature/iss/#2'
+      });
+    }else {
+      return Promise.resolve(undefined);
+    }
+  });
+  jest.spyOn(connectService, 'updateWorkspaceIssue').mockImplementation((issue)=>{
+    return Promise.resolve(issue);
+  });
+  jest.spyOn(connectService, 'createIssueRecord').mockImplementation((issue)=>Promise.resolve(undefined));
+  jest.spyOn(gitPlatformService, 'createPR').mockImplementation((issue)=>{
+    const updated = {
+      ...issue,
+      gitPrUrl: "https://github.com/fika-dev/fika-cli-test-samples/pull/3"
+    }
+    return Promise.resolve(updated);
+  });
 });
 
 beforeEach(async()=>{
-  // jest.restoreAllMocks();
-  // jest.spyOn(process.stdout, "write").mockImplementation(()=>true);
-  // jest.spyOn(console, "log").mockImplementation(()=>true);
-  // jest.spyOn(messageService, 'showSuccess').mockImplementation(()=>{});
-  // jest.spyOn(configService, 'getWorkspaceId').mockImplementation(()=>new Uuid('d3224eba-6e67-4730-9b6f-a9ef1dc7e4ac'));
-  // jest.spyOn(gitPlatformService, 'createPR').mockImplementation((issue)=>{
-  //   const updated = {
-  //     ...issue,
-  //     gitPrUrl: 'https://github.com/fika-dev/fika-cli-test-samples/pull/1502'
-  //   }
-  //   return Promise.resolve(updated);
-  // });
-  // await gitPlatformService.checkoutToBranchWithoutReset('develop');
-  // await restoreGitRepo(process.env.TESTING_REPO_PATH);
 });
 
 afterEach(async ()=>{
@@ -51,81 +97,60 @@ afterAll(() => {
   
 });
 
-// jest test function for only true case
-test("finish action", async () => {
-  // await finishAction();
-});
+test("1. finish with uncommited changes", async ()=>{
+  const spy = jest.spyOn(promptService, "confirmAction").mockImplementation(()=>Promise.resolve(true));
+  spyWithMock(defaultMock((cmd: GitCommand) => {
+    if (cmd.command === statusCmd.command){
+      return T.of(TEST_GIT_STATUS_WITH_STAGED);
+    }
+    return undefined;
+  }))
+  await finishAction();
+  expect(spy).toBeCalledTimes(2);
+})
 
-// it("2.test finish without change & without merge check, without checkout", async () => {
-//   try {
-//     await gitPlatformService.checkoutToBranchWithoutReset(TEST_CPR_BRANCH_NAME);
-//     const localConfig = defaultLocalConfig;
-//     localConfig.finish.checkMergeConflict = false;
-//     jest.spyOn(configService, 'getLocalConfig').mockImplementation(() => localConfig);
-//     jest.spyOn(promptService, 'confirmAction').mockImplementation((message: string) => message.includes("Do you wanna stay ")? Promise.resolve(true): Promise.resolve(false));
-//     const spy = jest.spyOn(messageService, 'showSuccess').mockImplementation(()=>{});
-//     await finishAction();
-//     expect(spy).toBeCalled();
-//     const currentBranch = await gitPlatformService.getBranchName();
-//     expect(currentBranch).toBe(TEST_CPR_BRANCH_NAME);
-//     await deleteBranch(TEST_CPR_BRANCH_NAME);  
-//   } catch (e) {
-//     // await deleteBranch(TEST_CPR_BRANCH_NAME);  
-//   }
-// });
+test("2. finish from develop branch (after commit)", async ()=>{
+  spyWithMock(defaultMock((cmd: GitCommand) => {
+    if (cmd.command === getCurrentBranchCmd.command){
+      return T.of('develop');
+    }
+    return undefined;
+  }))
+  try{
+    await finishAction();
+    expect(true).toEqual(false);
+  }catch(e){
+    expect(e.type).toEqual('IssueRecordNotFound');
+    expect(e.value).toEqual('develop');
+  }
+})
 
-// it("3.test finish without change & without merge check, with checkout", async () => {
-  
-//   await gitPlatformService.checkoutToBranchWithoutReset(TEST_CPR_BRANCH_NAME);
-//   const localConfig = defaultLocalConfig;
-//   localConfig.finish.checkMergeConflict = false;
-//   localConfig.finish.checkOutToDevelop = true;
-//   jest.spyOn(configService, 'getLocalConfig').mockImplementation(() => localConfig);
-//   const spy = jest.spyOn(messageService, 'showSuccess').mockImplementation(()=>{});
-//   await finishAction();
-//   expect(spy).toBeCalled();
-//   const currentBranch = await gitPlatformService.getBranchName();
-//   expect(currentBranch).toBe('develop');
-//   await deleteBranch(TEST_CPR_BRANCH_NAME);  
-  
-  
-// });
+test("3. finish but there is a conflict after pulling", async ()=>{
+  spyWithMock(defaultMock((cmd: GitCommand) => {
+    if (cmd.command === pullFromCmd.command){
+      return T.of(TEST_GIT_PULL_CONFLICT_OUTPUT);
+    }
+    return undefined;
+  }))
+  try{
+    await finishAction();
+    expect(true).toEqual(false);
+  }catch(e){
+    expect(e.type).toEqual('GitErrorMergeConflict');
+  }
+})
 
-// it("4. checkOutToDevelop is false & confirm to stay", async () => {
-//     await gitPlatformService.checkoutToBranchWithoutReset(TEST_CPR_BRANCH_NAME);
-//     const localConfig = defaultLocalConfig;
-//     localConfig.finish.checkOutToDevelop = false;
-//     jest.spyOn(configService, 'getLocalConfig').mockImplementation(() => localConfig);
-//     jest.spyOn(promptService, 'confirmAction').mockImplementation(() => Promise.resolve(true));
-//     const spy = jest.spyOn(messageService, 'showSuccess').mockImplementation(()=>{});
-//     await finishAction();
-//     expect(spy).toHaveBeenLastCalledWith("Staying in the current branch");
-//     await deleteBranch(TEST_CPR_BRANCH_NAME);  
-// });
+// test("4. finish issue which has been deleted from the workspace", async ()=>{})
 
-// it("5. checkOutToDevelop is false & confirm to checkout to dev", async () => {
-  
-//     await gitPlatformService.checkoutToBranchWithoutReset(TEST_CPR_BRANCH_NAME);
-//     const localConfig = defaultLocalConfig;
-//     localConfig.finish.checkOutToDevelop = false;
-//     jest.spyOn(configService, 'getLocalConfig').mockImplementation(() => localConfig);
-//     jest.spyOn(promptService, 'confirmAction').mockImplementation((message: string) => message.includes("Do you wanna stay ")? Promise.resolve(false): Promise.resolve(false));
-//     await finishAction();
-//     const currentBranch = await gitPlatformService.getBranchName();
-//     expect(currentBranch).toBe('develop');
-//     await deleteBranch(TEST_CPR_BRANCH_NAME);  
-  
-// });
+// test("5. finish without git remote url", async ()=>{})
 
-// it("6. test when PR is already opened", async () => {
-//     await gitPlatformService.checkoutToBranchWithoutReset(TEST_CPR_BRANCH_NAME);
-//     jest.spyOn(gitPlatformService, 'createPR').mockImplementation(() => {
-//       throw new GhPrAlreadyExists("GhPrAlreadyExists");
-//     });
-//     const spy = jest.spyOn(messageService, 'showSuccess').mockImplementation(() => {})
-//   try {
-//     await createPR();  
-//   } catch (e) {
-//     expect(spy).toBeCalledWith("PR link", undefined,'https://github.com/fika-dev/fika-cli-test-samples/pull/12');  
-//   }
-// });
+// test("6. finish with wrong remote url", async ()=>{})
+
+test("7. Sean finish and everything works as expected", async ()=>{
+  const spy = jest.spyOn(messageService, 'showSuccess').mockImplementation(()=>{});
+  spyWithMock(defaultMock((_: GitCommand) => undefined));
+  await finishAction();
+  expect(spy).toHaveBeenNthCalledWith(1, "Synced from origin develop");
+  expect(spy).toHaveBeenNthCalledWith(2, "Pull Request Created", undefined, "https://github.com/fika-dev/fika-cli-test-samples/pull/3");
+  expect(spy).toHaveBeenNthCalledWith(3, "Notion Issue Updated", undefined, "https://www.notion.so/test-fika-start-doc-4af459df4efb448483fe3e2b703d50fd");
+})
