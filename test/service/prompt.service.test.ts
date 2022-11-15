047f607ae1a3a507b9d1ea35274eb461d3efbe95
tuxshido <c.metman@e.email>
2022-11-15 17:38:03 +0900

    [test] for the yes flag

---
+++ test/service/prompt.service.test.ts
@@ -0,0 +1,39 @@
+import SERVICE_IDENTIFIER from "@/config/constants/identifiers";
+import container from "@/config/ioc_config";
+import { IPromptService } from "@/domain/service/i-prompt.service";
+import promptly from "promptly";
+const promptService = container.get<IPromptService>(SERVICE_IDENTIFIER.PromptService);
+
+beforeAll(async () => {
+  jest.restoreAllMocks();
+});
+
+beforeEach(async()=>{
+  
+});
+
+afterEach(async ()=>{
+  
+
+})
+
+afterAll(() => {
+  
+});
+
+describe("1. test confirmAction", () => {
+    test("1.1. test confirm action without flag", async () => {
+        const spy = jest.spyOn(promptly, "confirm").mockImplementation(()=>Promise.resolve(false));
+        const answer = await promptService.confirmAction("any message");
+        expect(answer).toBe(false);
+        expect(spy).toHaveBeenCalledTimes(1);
+    });
+    
+    test("1.2. test confirm action with flag", async () => {
+        promptService.setAcceptsAllPromptsAsYes();
+        const spy = jest.spyOn(promptly, "confirm").mockImplementation(()=>Promise.resolve(false));
+        const answer = await promptService.confirmAction("any message");
+        expect(answer).toBe(true);
+        expect(spy).toHaveBeenCalledTimes(0);
+  });
+});
