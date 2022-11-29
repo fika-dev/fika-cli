91f2bd4f42cb17904de7883a5eb57146f33333ac
tuxshido <c.metman@e.email>
2022-11-29 17:19:41 +0900

    [faet] add a select option to switch  between workspace

--- src/command/set/set.action.ts
+++ src/command/set/set.action.ts
@@ -1,25 +1,25 @@
-import { WorkspaceType } from "@/domain/entity/add_on/workspace_platform.entity";
-import { IMessageService } from "@/domain/service/i_message.service";
-import { Uuid } from "@/domain/value_object/uuid.vo";
 import SERVICE_IDENTIFIER from "src/config/constants/identifiers";
 import container from "src/config/ioc_config";
 import { IConfigService } from "src/domain/service/i_config.service";
-import { IConnectService } from "src/domain/service/i_connect.service";
+import {
+  switchNoWorkspace,
+  switchOneWorkspace,
+  switchMultiWorkspace,
+  setNewWorkspace,
+} from "@/actions/fika/set-workspace.action";
 
-export const setAction = async (workspaceTypeAndId: string) => {
+export const setAction = async (workspaceTypeAndId?: string) => {
   const configService = container.get<IConfigService>(SERVICE_IDENTIFIER.ConfigService);
-  const messageService = container.get<IMessageService>(SERVICE_IDENTIFIER.MessageService);
-  const connectService = container.get<IConnectService>(SERVICE_IDENTIFIER.ConnectService);
-  const [workspaceType, idString] = workspaceTypeAndId.split(":");
-  const workspaceId: Uuid = new Uuid(idString);
-  const workspace = await connectService.requestWorkspace(
-    workspaceType as WorkspaceType,
-    workspaceId
-  );
-  configService.updateWorkspace(workspace);
-  await messageService.showSuccess(
-    "Notion is connected successfully",
-    "Fika Quick Start Documentation",
-    "https://blog.fikadev.com/posts/start-fika"
-  );
+  if (workspaceTypeAndId === undefined) {
+    const workspaceList = configService.getWorkspaceList();
+    if (workspaceList === "NOT_CONNECTED") {
+      await switchNoWorkspace();
+    } else if (workspaceList.length === 1) {
+      await switchOneWorkspace(workspaceList[0]);
+    } else {
+      await switchMultiWorkspace(workspaceList);
+    }
+  } else {
+    await setNewWorkspace(workspaceTypeAndId);
+  }
 };
